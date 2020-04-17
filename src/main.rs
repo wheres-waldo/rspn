346fc92bbeba919abfcab9baa0f9aae7233feec0
Steven Waldron <steven.waldron@protonmail.com>
2020-04-17 16:28:41 -0500

    implemented rpn calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let calculation = &args[1..];
+    let mut stack: Vec<i32> = vec![];
+
+    calculation.iter().for_each(|arg| {
+        if let Ok(i) = arg.parse::<i32>() {
+            stack.push(i);
+        } else {
+            match arg.as_bytes() {
+                [b'+'] => {
+                    if let Some(i) = stack.pop() {
+                        if let Some(j) = stack.pop() {
+                            stack.push(i + j);
+                        } else {
+                            panic!("Not enough operands");
+                        }
+                    } else {
+                        panic!("Not enough operands");
+                    }
+                }
+                [b'-'] => {
+                    if let Some(i) = stack.pop() {
+                        if let Some(j) = stack.pop() {
+                            stack.push(i - j);
+                        } else {
+                            panic!("Not enough operands");
+                        }
+                    } else {
+                        panic!("Not enough operands");
+                    }
+                },
+                [b'/'] => {
+                    if let Some(i) = stack.pop() {
+                        if let Some(j) = stack.pop() {
+                            stack.push(i / j);
+                        } else {
+                            panic!("Not enough operands");
+                        }
+                    } else {
+                        panic!("Not enough operands");
+                    }
+                },
+                [b'*'] => {
+                    if let Some(i) = stack.pop() {
+                        if let Some(j) = stack.pop() {
+                            stack.push(i * j);
+                        } else {
+                            panic!("Not enough operands");
+                        }
+                    } else {
+                        panic!("Not enough operands");
+                    }
+                },
+                _ => panic!("WTF ARE YOU FEEDING ME!!")
+            }
+        }
+    });
+
+    println!("{}", stack.pop().unwrap())
 }
